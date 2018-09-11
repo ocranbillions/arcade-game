27160d587866d0c196607b0b93bbf2265405662b
Samuel Ocran <sam.ocran@wakanow.local>
2018-09-11 21:20:03 +0100

    Enemy movement re-implemented

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    let verticalAlignCenter = -10;
+    this.x = x;
+    this.y = verticalAlignCenter + y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //Size of each tile
+    this.tileWidth = 101;
+
+    //Distance at which enemy walks off the game board
+    this.offScreen = this.tileWidth * 5;
+
+    if(this.x < this.offScreen){
+        //move enemy
+        this.x += 200 * dt;
+    }else{
+        //walk in from the left
+        //if enemy crosses the end of the board
+        this.x = -this.tileWidth;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -83,6 +101,14 @@ class Hero {
 // Place the player object in a variable called player
 
 const player = new Hero();
+const enemy1 = new Enemy(-401, 83);
+const enemy2 = new Enemy(-101, 166);
+const enemy3 = new Enemy(-101, 249);
+const enemy4 = new Enemy(-245, 249);
+
+let allEnemies = [];
+allEnemies.push(enemy1, enemy2, enemy3, enemy4);
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
